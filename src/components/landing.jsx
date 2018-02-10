378acd523a1da6ce4ab5d03dbd24a5031bfb5298
spdev <spdevuk@gmail.com>
2018-02-10 12:52:22 +0000

    Got storeId from input

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,15 +2,31 @@ import React, {Component} from 'react';
 
 //css
 import '../styles/landingpage.css';
+//helper
+var h = require('../scripts/helpers');
+
+//variables
+let storeId = null;
+
+
 
 class Landing extends Component {
+
+  goToStore(event) {
+    event.preventDefault();
+    //get the dtata from input
+    // var StoreID = this.refs.storeId.value;
+    console.log(storeId.value);
+    //transition from Landing to Home
+  }
+  
   render() {
     return (
       <div id="landing">
         <div className="container-fluid">
           <div className="storesearch">
-            <form>
-              <input type="text" placeholder="example: My cool store!"/>
+            <form className="store-selector" onSubmit={this.goToStore}>
+              <input type="text" ref={(input) => { storeId = input }} defaultValue={h.getFunName()}/>
             <h3>
               Enter A Store Name
             </h3>
